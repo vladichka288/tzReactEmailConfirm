555634b522a9023fa8cabbb8c502ba8b9c064e26
vladichka <vladislavovish596@gmail.com>
2021-02-21 22:25:15 +0200

    Form created and validation

--- src/Registration/Form/Form.js
+++ src/Registration/Form/Form.js
@@ -1,12 +1,52 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
+import { css } from "@emotion/css";
 import * as classes from "./style.js";
+var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export default function Form() {
-  const email = useRef();
-  const password = useRef();
+  const [passwordValidity, setPasswordValidity] = useState(false);
+  const [emailValidity, setEmailValidity] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const changePasswordHandler = (event) => {
+    setPassword(event.target.value);
+    if (event.target.value < 8) {
+      setPasswordValidity(false);
+    } else {
+      setPasswordValidity(true);
+    }
+  };
+  const changeEmailHandler = (event) => {
+    setEmail(event.target.value);
+    if (!re.test(String(event.target.value).toLowerCase())) {
+      setEmailValidity(false);
+    } else {
+      setEmailValidity(true);
+    }
+  };
   const submitHandler = (event) => {
     event.preventDefault();
+    if (emailValidity && passwordValidity) {
+      console.log("success");
+    } else {
+      console.log("bad");
+    }
   };
+  let passwordHintStyle = css`
+    color: ${passwordValidity ? "rgba(37, 67, 83, 0.75)" : "#d54e4e;"};
+  `;
+  let InputField = css`
+    width: 100%;
+    padding: 10px 20px;
+    font-weight: 500;
+    font-size: 14px;
+    color: rgba(37, 67, 83, 0.4);
+    font-family: Montserrat;
+    border: 0.5px solid #c7c7c7;
+    box-sizing: border-box;
+    border-radius: 3px;
+    border: 2px solid ${emailValidity ? "" : " #d54e4e;"};
+  `;
   return (
     <form className={classes.FormContainer}>
       <style>
@@ -20,15 +60,27 @@ export default function Form() {
             Email
           </label>
         </div>
-        <input className={classes.InputField} type="email" id="email" />
+        <input
+          value={email}
+          onChange={changeEmailHandler}
+          className={InputField}
+          type="email"
+          id="email"
+        />
       </div>
       <div style={{ marginBottom: "30px" }}>
         <div>
           <label className={classes.Label} for="password">
-            Password <text>(min 8 symbols)</text>
+            Password <text className={passwordHintStyle}>(min 8 symbols)</text>
           </label>
         </div>
-        <input className={classes.InputField} type="password" id="password" />
+        <input
+          value={password}
+          onChange={changePasswordHandler}
+          className={classes.InputFieldPassword}
+          type="password"
+          id="password"
+        />
       </div>
       <button onClick={submitHandler} className={classes.SubmitButton}>
         Create account
--- src/Registration/Form/style.js
+++ src/Registration/Form/style.js
@@ -15,6 +15,20 @@ export const InputField = css`
   box-sizing: border-box;
   border-radius: 3px;
 `;
+export const InputFieldPassword = css`
+  width: 100%;
+  padding: 10px 20px;
+  font-weight: 500;
+  font-size: 14px;
+  color: rgba(37, 67, 83, 0.4);
+  font-family: Montserrat;
+  border-radius: 3px;
+  border: 0.5px solid #c7c7c7;
+  box-sizing: border-box;
+  border-radius: 3px;
+  font-weight: 900;
+  letter-spacing: 0.15em;
+`;
 export const Label = css`
   font-weight: 600;
   font-size: 14px;
@@ -22,13 +36,14 @@ export const Label = css`
   font-family: "Montserrat", sans-serif;
 `;
 export const SubmitButton = css`
-width:100%;
-background: #57B3E4;
-border-radius: 35px;
-font-size: 18px;
-color: #FFFFFF;
-font-family: Montserrat;
-border:0px;
-padding:14px;
-font-weight:700;
+  width: 100%;
+  background: #57b3e4;
+  border-radius: 35px;
+  font-size: 18px;
+  color: #ffffff;
+  font-family: Montserrat;
+  border: 0px;
+  padding: 14px;
+  font-weight: 700;
+  cursor: pointer;
 `;
